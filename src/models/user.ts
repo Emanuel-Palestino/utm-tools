923e3fb4af65b7fcc1b3333970ddbebbe6e50343
Raul Ruvalcaba <corvohyatt@gmail.com>
2023-09-19 12:41:26 -0600

    careers and user model added

---
+++ src/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+	name: string;
+	tuition: string; //matricula
+	semester: string;
+	disability: boolean;
+	desabilityDescription: string;
+	indigenousLanguage: boolean;
+	typeOfIndigenousLanguage: String;
+	phone: string;
+	email: string;
+}
---
+++ src/utils/careers.ts
@@ -0,0 +1,14 @@
+const careers:string[] = [
+	"Ingeniería en Computación",
+	"Ingeniería en Electrónica",
+	"Ingeniería en Diseño",
+	"Licenciatura en Ciencias Empresariales",
+	"Licenciatura en Matemáticas Aplicadas",
+	"Ingeniería en Alimentos",
+	"Ingeniería Industrial",
+	"Licenciatura en Estudios Mexicanos",
+	"Ingeniería en Mecatrónica",
+	"Ingeniería en Física Aplicada",
+	"Ingeniería en Mecánica Automotriz",
+	"Ingeniería Civil"
+]
