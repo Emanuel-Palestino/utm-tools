cfe5be33f093ecb8fa25e3b603c6b4abfc4c5b73
Roberto <67120473+s100tist@users.noreply.github.com>
2023-09-30 16:54:42 -0600

    Update PDFGenerator.tsx
    Attribute 'method' in options 'corrected'

--- src/utils/PDFGenerator.tsx
+++ src/utils/PDFGenerator.tsx
@@ -7,7 +7,7 @@ interface optionsProps
    options:
    {
       // default is `save`
-      method: 'open',
+      method: 'save',
       // default is Resolution.MEDIUM = 3, which should be enough, higher values
       // increases the image quality but also the size of the PDF, so be careful
       // using values higher than 10 when having multiple pages generated, it
