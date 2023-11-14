f43b45d9f4eb26cad7c76076a0e9498d942a205e
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2023-11-14 09:23:03 -0600

    Created company model and modified internship model.

--- src/models/Internship.ts
+++ src/models/Internship.ts
@@ -1,23 +1,10 @@
-import { CompanySector } from "./CompanySector"
+import { Company } from "./Company"
 import { InternshipPeriod } from "./InternshipPeriod"
 import { InternshipStudent } from "./InternshipStudent"
 import { Person } from "./Person"
 
 
 export interface Internship {
-	isInternacional: boolean		// ¿Es internacional?
-	sector: CompanySector			// Sector
-	industry: string				// Industria (Giro de la empresa)
-	companyName: string				// Nombre de la empresa
-	address: string					// Dirección
-	phone: string					// Teléfono
-	//phoneExtension: string			// Extensión
-	email: string					// Correo electrónico
-	webPage: string					// Página web
-	companyContact: string			// Contacto en la empresa
-	recipientName: string			// Nombre del destinatario
-	recipientPosition: string		// Cargo del destinatario
-	inAtentionOf: string			// A la atención de
 	applicationDate: string			// Fecha de solicitud
 
 	person: Person
@@ -25,4 +12,6 @@ export interface Internship {
 	student: InternshipStudent
 
 	period: InternshipPeriod
+
+	company: Company
 }
