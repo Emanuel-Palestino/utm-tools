2e1ae8e9f8f2ed812b108ef239f27a9d9963da2a
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2023-11-26 12:31:48 -0600

    Created state storage for social service data.

---
+++ app/store/socialService.ts
@@ -0,0 +1,66 @@
+import { Person } from "@/src/models/Person"
+import { GovernmentAgency } from "@/src/models/social_service/GovernmentAgency"
+import { SocialServicePeriod } from "@/src/models/social_service/SocialServicePeriod"
+import { SocialServiceStudent } from "@/src/models/social_service/SocialServiceStudent"
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+import { createStorage } from "../utils/constants"
+
+
+interface SocialServiceStore {
+	isPersonalDataComplete: boolean
+	personalData: Person | undefined
+
+	isStudentDataComplete: boolean
+	studentData: SocialServiceStudent | undefined
+
+	isPeriodDataComplete: boolean
+	periodData: SocialServicePeriod | undefined
+
+	isGovernmentAgencyDataComplete: boolean
+	governmentAgencyData: GovernmentAgency | undefined
+
+	setPersonalData: (personalData: Person) => void
+	setStudentData: (studentData: SocialServiceStudent) => void
+	setPeriodData: (periodData: SocialServicePeriod) => void
+	setGovernmentAgencyData: (governmentAgencyData: GovernmentAgency) => void
+
+	documentsDownloaded: { [key: string]: boolean }
+	setDocumentDownloaded: (key: string, value: boolean) => void
+}
+
+export const useSocialServiceStore = create<SocialServiceStore>()(
+	persist(
+		(set, get) => ({
+			isPersonalDataComplete: false,
+			personalData: undefined,
+
+			isStudentDataComplete: false,
+			studentData: undefined,
+
+			isPeriodDataComplete: false,
+			periodData: undefined,
+
+			isGovernmentAgencyDataComplete: false,
+			governmentAgencyData: undefined,
+
+			setPersonalData: (personalData: Person) => set(() => ({ personalData, isPersonalDataComplete: true })),
+
+			setStudentData: (studentData: SocialServiceStudent) => set(() => ({ studentData, isStudentDataComplete: true })),
+
+			setPeriodData: (periodData: SocialServicePeriod) => set(() => ({ periodData, isPeriodDataComplete: true })),
+
+			setGovernmentAgencyData: (governmentAgencyData: GovernmentAgency) => set(() => ({ governmentAgencyData, isGovernmentAgencyDataComplete: true })),
+
+			documentsDownloaded: {},
+			setDocumentDownloaded(key, value) {
+				set(() => ({ documentsDownloaded: { ...get().documentsDownloaded, [key]: value } }))
+			}
+		}),
+		{
+			name: 'social-service-storage',
+			storage: createStorage<SocialServiceStore>()
+		}
+
+	)
+)
