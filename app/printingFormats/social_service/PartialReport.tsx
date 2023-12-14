26b1bd449b4808b6da641d1d03951b94a1c57fea
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2023-12-13 20:00:16 -0600

    [Social Service] Filled partial report view with real data.

--- app/printingFormats/social_service/PartialReport.tsx
+++ app/printingFormats/social_service/PartialReport.tsx
@@ -1,8 +1,32 @@
 import Image from "next/image"
 import logo from '@/public/UTMEscudo.png'
+import { Person } from "@/src/models/Person"
+import { SocialServiceStudent } from "@/src/models/social_service/SocialServiceStudent"
+import { GovernmentAgency } from "@/src/models/social_service/GovernmentAgency"
+import { Activity, SocialServicePeriod } from "@/src/models/social_service/SocialServicePeriod"
+import { FC } from "react"
+import { formatedDate } from "@/app/utils/format"
 
 
-export const PartialReport = () => {
+interface PartialReportProps {
+	person: Person
+	student: SocialServiceStudent
+	governmentAgency: GovernmentAgency
+	period: SocialServicePeriod
+	formatNumber: number
+	activity: Activity
+	description: string
+}
+
+export const PartialReport: FC<PartialReportProps> = ({
+	person,
+	student,
+	governmentAgency,
+	period,
+	formatNumber,
+	activity,
+	description
+}) => {
 
 	return (
 		<section className="w-full h-full text-[14px] flex flex-col justify-between">
@@ -15,83 +39,83 @@ export const PartialReport = () => {
 					</div>
 				</div>
 
-				<p className="text-right mt-8">Formato No. 1</p>
+				<p className="text-right mt-8">Formato No. {formatNumber}</p>
 
 				<div className="flex flex-col gap-1 mt-4">
 					<div className="flex gap-2 items-center">
 						<p className="text-left w-[180px] font-semibold">NOMBRE COMPLETO DEL ALUMNO:</p>
-						<p>Emanuel Palestino Hernández</p>
+						<p>{`${person.paternalSurname} ${person.maternalSurname} ${person.name}`}</p>
 					</div>
 
 					<div className="flex gap-6">
 						<div className="flex gap-2">
 							<p className="text-left w-[180px] font-semibold">CARRERA:</p>
-							<p>Ingeniería en Computación</p>
+							<p>{student.career}</p>
 						</div>
 
 						<div className="flex gap-2">
 							<p className="font-semibold">SEMESTRE:</p>
-							<p>Octavo</p>
+							<p>{student.semester}</p>
 						</div>
 
 						<div className="flex gap-2">
 							<p className="font-semibold">MATRÍCULA:</p>
-							<p>239847293784</p>
+							<p>{student.enrollment}</p>
 						</div>
 					</div>
 
 					<div className="flex gap-2">
 						<p className="text-left w-[180px] font-semibold">RESPONSABLE INMEDIATO:</p>
-						<p>Emanuel palestino Hernández</p>
+						<p>{governmentAgency.supervisorName}</p>
 					</div>
 
 					<div className="flex gap-2 items-center">
 						<p className="text-left w-[180px] font-semibold">INSTITUCIÓN Y/O DEPENDENCIA:</p>
-						<p>Municipio de oaxaca oaxaca</p>
+						<p>{governmentAgency.name}</p>
 					</div>
 
 					<div className="flex gap-2">
 						<p className="text-left w-[180px] font-semibold">ÁREA O DEPARTAMENTO:</p>
-						<p>Estadística y Computación</p>
+						<p>{governmentAgency.supervisorWorkArea}</p>
 					</div>
 
 					<div className="flex gap-2">
 						<p className="text-left w-[180px] font-semibold">DIRECCIÓN:</p>
-						<p>calle 1 oaxaca oaxaca huajuapan</p>
+						<p>{governmentAgency.address}</p>
 					</div>
 
 					<div className="flex gap-2">
 						<p className="text-left w-[180px] font-semibold">CIUDAD O LOCALIDAD:</p>
-						<p>Huajuapan de leon</p>
+						<p>{governmentAgency.city}</p>
 					</div>
 
 					<div className="flex gap-6">
 						<div className="flex gap-2">
 							<p className="text-left w-[180px] font-semibold">ESTADO:</p>
-							<p>Oaxaca</p>
+							<p>{governmentAgency.state}</p>
 						</div>
 
 						<div className="flex gap-2">
 							<p className="text-left font-semibold">TELÉFONO:</p>
-							<p>95323423424</p>
+							<p>{governmentAgency.supervisorPhone}</p>
 						</div>
 					</div>
 
 					<div className="flex gap-6">
 						<div className="flex gap-2">
 							<p className="text-left w-[180px] font-semibold">HORARIO:</p>
-							<p>De 9am a 10pm</p>
+							<p>{period.schedule}</p>
 						</div>
 
 						<div className="flex gap-2">
 							<p className="font-semibold">TOTAL DE HORAS REALIZADAS:</p>
-							<p>555</p>
+							<p>{activity.hours}</p>
 						</div>
 					</div>
 
 					<div className="flex gap-2">
 						<p className="text-left w-[180px] font-semibold">PERIODO:</p>
-						<p>Del 5 de octubre del 2023 a 5 de octubre del 2023</p>
+						<p>Del {formatedDate(activity.startDate)} al {formatedDate(activity.endDate)}</p>
 					</div>
 				</div>
 
@@ -105,10 +129,19 @@ export const PartialReport = () => {
 						</thead>
 
 						<tbody>
-							<tr>
-								<td className="border-b border-r border-black p-2">1.-</td>
-								<td className="border-b border-r border-black p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique porro doloribus, deleniti commodi earum odit unde itaque quia, est, iusto voluptates tempore. Eos soluta praesentium quasi provident neque, impedit maiores!</td>
-							</tr>
+							{
+								description.split('\n').map((paragraph, index) => {
+									if (!paragraph)
+										return null
+
+									return (
+										<tr key={`activity-description-${index}`}>
+											<td className="border-b border-r border-black p-2">{index + 1}.-</td>
+											<td className="border-b border-r border-black p-2">{paragraph}</td>
+										</tr>
+									)
+								})
+							}
 						</tbody>
 					</table>
 				</div>
