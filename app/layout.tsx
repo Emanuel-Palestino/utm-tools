71698de08abe29b6c929bcb30b03d1a1304a35e3
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2023-12-18 11:46:45 -0600

    Added vercel speed insights.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import '@/app/global.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
 			<body>
 				{children}
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	)
