fe69c044ab9048b15c668a80aadb1e24086f8317
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2024-01-21 20:02:17 -0600

    Fixed responsive styles of navbar.

--- app/ui/Navigation.tsx
+++ app/ui/Navigation.tsx
@@ -30,27 +30,27 @@ export const Navigation = () => {
 			<NavbarBrand>
 				<Link href="/" className="flex items-center gap-1 sm:gap-2">
 					<Image className="hidden sm:block" src={logo} alt="Logo" width={50} height={50} />
-					<Image className="block sm:hidden" src={logo} alt="Logo" width={36} height={36} />
-					<p className="hidden sm:block font-bold text-xl text-inherit">Herramientas UTM</p>
+					<Image className="block sm:hidden" src={logo} alt="Logo" width={42} height={42} />
+					<p className="hidden md:block font-bold text-xl text-inherit">Herramientas UTM</p>
 				</Link>
 			</NavbarBrand>
 
 			<NavbarContent justify="center">
-				<h2 className="text-2xl">{routerTitles[router]}</h2>
+				<h2 className="text-xl sm:text-2xl">{routerTitles[router]}</h2>
 			</NavbarContent>
 
-			<NavbarContent className="flex gap-2 md:gap-6" justify="end">
+			<NavbarContent className="flex gap-3 md:gap-6" justify="end">
 				<NavbarItem>
 					<UILink color="foreground" href="mailto:emanuel.palestino.h@gmail.com" className="flex items-center gap-1">
 						<EmailIcon />
-						<span className="hidden sm:block h-[26px]">Contacto</span>
+						<span className="hidden md:block h-[26px]">Contacto</span>
 					</UILink>
 				</NavbarItem>
 
 				<NavbarItem>
 					<UILink color="foreground" isExternal href="https://github.com/Emanuel-Palestino/utm-tools" className="flex items-center gap-1">
 						<GitHubIcon />
-						<span className="hidden sm:block h-[26px]">Contribuir</span>
+						<span className="hidden md:block h-[26px]">Contribuir</span>
 					</UILink>
 				</NavbarItem>
 			</NavbarContent>
