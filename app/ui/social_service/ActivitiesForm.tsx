7188177b3e07075c1818bf1afbea759057eaacb3
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2024-09-22 14:41:14 -0600

    [social service] the activities can be added manually, dates and hours activities are updated automatically.

--- app/ui/social_service/ActivitiesForm.tsx
+++ app/ui/social_service/ActivitiesForm.tsx
@@ -1,122 +1,189 @@
 import { Activity } from "@/src/models/social_service/SocialServicePeriod"
-import { Card, CardBody } from "@nextui-org/card"
+import { Card, CardBody, CardHeader } from "@nextui-org/card"
 import { Input, Textarea } from "@nextui-org/input"
 import { Button } from "@nextui-org/button"
 import { Controller, useFieldArray, useForm } from "react-hook-form"
 import { useSocialServiceStore } from "@/app/store/socialService"
+import { MinusIcon } from "@/app/icons"
+import { addDays, differenceInBusinessDays, formatISO, parseISO } from "date-fns"
 
 
 const ActivitiesForm = () => {
 
-	const { activities, save, isComplete, periodMonths } = useSocialServiceStore(state => ({
+	const { activities, save, isComplete, isPeriodComplete, periodData } = useSocialServiceStore(state => ({
 		save: state.setActivitiesData,
 		activities: state.activitiesData,
 		isComplete: state.isActivitiesDataComplete,
-		periodMonths: state.periodData?.months || 0
+		isPeriodComplete: state.isPeriodDataComplete,
+		periodData: {
+			schedules: state.periodData?.schedules || [[9, 18]],
+			startDate: state.periodData?.startDate || new Date(),
+			endDate: state.periodData?.endDate || new Date()
+		}
 	}))
 
-	const { handleSubmit, control } = useForm<{ activities: Activity[] }>({
+	const totalHoursPerDay = periodData.schedules.reduce((acc, [start, end]) => acc + (end - start), 0)
+
+	const { handleSubmit, control, getValues, setValue } = useForm<{ activities: Activity[] }>({
 		defaultValues: {
-			activities: Array.from({ length: periodMonths }, () => ({
+			activities: activities || [{
 				description: '',
-				startDate: new Date(),
-				endDate: new Date(),
-				hours: 0
-			}))
+				startDate: periodData.startDate,
+				endDate: periodData.endDate,
+				hours: totalHoursPerDay * differenceInBusinessDays(periodData.endDate, periodData.startDate)
+			}]
 		},
 		values: activities ? { activities } : undefined
 	})
 
-	const { fields } = useFieldArray({ control, name: 'activities' })
+	const { fields, append, remove } = useFieldArray({ control, name: 'activities' })
 
-	const onSubmit = handleSubmit(data => {
-		save(data.activities.map(activity => ({
-			...activity,
-			startDate: new Date(`${activity.startDate} UTC-6`),
-			endDate: new Date(`${activity.endDate} UTC-6`)
-		})))
-	})
+	const onEndDateChange = (currentEndDate: Date, index: number) => {
+		const currentStartDate = getValues(`activities.${index}.startDate`)
+
+		// If it's the last activity, only update the hours
+		setValue(
+			`activities.${index}.hours`,
+			totalHoursPerDay * differenceInBusinessDays(currentEndDate, currentStartDate)
+		)
+		if (index === fields.length - 1) return
+
+		// If it's not the last activity, update the start date and the hours of the next activity
+		const nextEndDate = getValues(`activities.${index + 1}.endDate`)
+
+		const nextStartDate = addDays(currentEndDate, 1)
+		setValue(`activities.${index + 1}.startDate`, nextStartDate)
+
+		const nextHours = totalHoursPerDay * differenceInBusinessDays(nextEndDate, nextStartDate)
+		setValue(`activities.${index + 1}.hours`, nextHours)
+	}
+
+	const onSubmit = handleSubmit(data => save(data.activities))
 
 	return (
 		<Card>
-			<CardBody className="p-4">
-				<ul className="p-2 pl-6 mb-3 text-sm text-gray-600 list-disc list-outside">
+			<CardHeader>
+				<ul className="pl-6 text-sm text-gray-600 list-disc list-outside">
 					<li>Las principales actividades a realizar en el servicio social.</li>
 					<li>Este <b>listado de actividades</b> aparecerá únicamente en el documento <b>cronograma de actividades</b>. Debido a esto la <b>descripción</b> debe ser <b>corta.</b></li>
-					<li>Las <b>fechas</b> se utilizarán para generar los <b>reportes parciales</b> (mensuales).</li>
+					<li><b>No es necesario</b> que el número de actividades sea igual al número de meses de duración del servicio social.</li>
 				</ul>
-				<form onSubmit={onSubmit} className="flex flex-col gap-6 md:gap-4">
-					{fields.map((_, index) => (
-						<div key={`activity_${index}`} className="w-full flex flex-col md:flex-row gap-2">
-							<Controller
-								name={`activities.${index}.description`}
-								control={control}
-								render={({ field }) => (
-									<Textarea
-										type="text"
-										label={`Descripción de la Actividad ${index + 1}`}
-										minRows={2}
-										{...field}
-										className="md:basis-1/2"
-									/>
-								)}
-							/>
+			</CardHeader>
 
-							<div className="flex flex-grow flex-wrap md:flex-nowrap gap-2">
-								<Controller
-									name={`activities.${index}.startDate`}
-									control={control}
-									render={({ field }) => (
-										<Input
-											type="date"
-											label="Fecha de Inicio"
-											isRequired
-											{...field}
-											value={field.value instanceof Date ? field.value.toISOString().split('T')[0] : field.value}
-										/>
-									)}
-								/>
-
-								<Controller
-									name={`activities.${index}.endDate`}
-									control={control}
-									render={({ field }) => (
-										<Input
-											type="date"
-											label="Fecha de Término"
-											isRequired
-											{...field}
-											value={field.value instanceof Date ? field.value.toISOString().split('T')[0] : field.value}
-										/>
-									)}
-								/>
-
-								<Controller
-									name={`activities.${index}.hours`}
-									control={control}
-									render={({ field }) => (
-										<Input
-											type="number"
-											label="Horas"
-											isRequired
-											min={1}
-											step={1}
-											{...field}
-											value={String(field.value)}
-										/>
-									)}
-								/>
-							</div>
-						</div>
-					))}
-
-					<div className="flex justify-center mt-2 md:col-span-2">
-						<Button className="w-32" color="primary" type="submit">
-							{isComplete ? 'Actualizar' : 'Guardar'}
+			{isPeriodComplete ?
+				<CardBody>
+					<section className="p-2 mb-1">
+						<Button
+							color="primary"
+							size="md"
+							onClick={() => append({
+								description: '',
+								startDate: getValues('activities')[fields.length - 1].endDate,
+								endDate: periodData.endDate,
+								hours: 0
+							})}
+						>
+							Agregar Actividad
 						</Button>
-					</div>
-				</form>
-			</CardBody>
+					</section>
+
+					<form onSubmit={onSubmit} className="flex flex-col gap-6 md:gap-4">
+						{fields.map((field, index) => (
+							<div key={field.id} className="w-full flex flex-col md:flex-row gap-2">
+								<Controller
+									name={`activities.${index}.description`}
+									control={control}
+									render={({ field }) => (
+										<Textarea
+											type="text"
+											label={`Descripción de la Actividad ${index + 1}`}
+											minRows={2}
+											{...field}
+											className="md:basis-1/2"
+										/>
+									)}
+								/>
+
+								<div className="flex flex-grow flex-wrap md:flex-nowrap gap-2 items-center">
+									<Controller
+										name={`activities.${index}.startDate`}
+										control={control}
+										render={({ field }) => (
+											<Input
+												type="date"
+												label="Fecha de Inicio"
+												isRequired
+												{...field}
+												value={field.value instanceof Date ? formatISO(field.value, { representation: 'date' }) : field.value}
+												readOnly
+												isReadOnly
+												isDisabled
+											/>
+										)}
+									/>
+
+									<Controller
+										name={`activities.${index}.endDate`}
+										control={control}
+										rules={{
+											onChange: e => onEndDateChange(e.target.value, index)
+										}}
+										render={({ field }) => (
+											<Input
+												type="date"
+												label="Fecha de Término"
+												isRequired
+												{...field}
+												// cast the value to a date object
+												onChange={e => field.onChange(parseISO(e.target.value))}
+												value={field.value instanceof Date ? formatISO(field.value, { representation: 'date' }) : field.value}
+											/>
+										)}
+									/>
+
+									<Controller
+										name={`activities.${index}.hours`}
+										control={control}
+										render={({ field }) => (
+											<Input
+												type="number"
+												label="Horas"
+												isRequired
+												min={1}
+												step={1}
+												{...field}
+												value={String(field.value)}
+											/>
+										)}
+									/>
+
+									{fields.length > 1 && (
+										<Button
+											isIconOnly
+											color="primary"
+											size="sm"
+											className="text-white"
+											onClick={() => remove(index)}
+										>
+											<MinusIcon />
+										</Button>
+									)}
+								</div>
+							</div>
+						))}
+
+						<div className="flex w-full justify-center md:col-span-2">
+							<Button className="w-32" color="primary" type="submit">
+								{isComplete ? 'Actualizar' : 'Guardar'}
+							</Button>
+						</div>
+					</form>
+				</CardBody>
+				:
+				<CardBody>
+					<p className="text-center text-2xl text-gray-700">Por favor completa la información del <b>periodo</b> para completar este formulario.</p>
+				</CardBody>
+			}
 		</Card>
 	)
 
