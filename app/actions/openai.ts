66ed8ad3afc6744690d301b35b390e863174ea5d
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2024-12-07 10:53:35 -0600

    Updated open ai api call

--- app/actions/openai.ts
+++ app/actions/openai.ts
@@ -9,29 +9,31 @@ export async function createAIModel(reports: string[]) {
 
 	try {
 		const reportCompletion = await openai.chat.completions.create({
+			model: "gpt-4o-mini",
 			messages: [
 				{
-					role: "user",
-					content: `Actua como un universitario que ha terminado sus estancias profesionales.
-					Crea un resumen de los siguientes textos.
-					Utiliza el mismo lenguaje utilizado en los textos originales o un poco más formal.
-					De ser necesario y posible, dividelo en párrafos pero no por etapas.
-					Sólo utiliza la palabra de estancias profesionales para referirte a esta actividad.
-					No dividas el resumen en etapas ni fases, simplemente en actividades realizadas.`
+					role: "system",
+					content: [{
+						type: "text",
+						text: `Eres un asistente que actua como un universitario que ha terminado sus estancias profesionales.
+									Crea un resumen de los siguientes textos utilizando el mismo lenguaje de los textos originales o un poco más formal.
+									De ser necesario y posible, dividelo en párrafos pero no por etapas.
+									Sólo utiliza la palabra de estancias profesionales para referirte a esta actividad.
+									No dividas el resumen en etapas ni fases, simplemente en actividades realizadas.`
+					}]
 				},
 				{
 					role: "user",
 					content: reports.join("\n\n")
 				}
 			],
-			model: "gpt-3.5-turbo",
-			max_tokens: 300
 		})
 
 		return {
 			message: reportCompletion.choices[0].message.content
 		}
 	} catch (err) {
+		console.error(err)
 		return {
 			error: 'Ocurrió un error al ejecutar la petición'
 		}
