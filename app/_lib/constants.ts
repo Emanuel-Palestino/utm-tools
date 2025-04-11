f0761a3dd547ce6c88d496c46c3017570735c2d1
Emanuel Palestino <75344407+Emanuel-Palestino@users.noreply.github.com>
2025-04-11 11:42:48 -0600

    update of internship periods

--- app/_lib/constants.ts
+++ app/_lib/constants.ts
@@ -1,23 +1,23 @@
 // Months start at 0
 export const INTERNSHIP_PERIODS = [
   {
-    startDate: new Date(2024, 6, 1).getTime(),
-    endDate: new Date(2024, 7, 16).getTime(),
+    startDate: new Date(2025, 6, 1).getTime(),
+    endDate: new Date(2025, 7, 18).getTime(),
   },
   {
-    startDate: new Date(2024, 6, 8).getTime(),
-    endDate: new Date(2024, 7, 23).getTime(),
+    startDate: new Date(2025, 6, 2).getTime(),
+    endDate: new Date(2025, 7, 19).getTime(),
   },
   {
-    startDate: new Date(2024, 6, 15).getTime(),
-    endDate: new Date(2024, 7, 30).getTime(),
+    startDate: new Date(2025, 6, 3).getTime(),
+    endDate: new Date(2025, 7, 20).getTime(),
   },
   {
-    startDate: new Date(2024, 6, 22).getTime(),
-    endDate: new Date(2024, 8, 6).getTime(),
+    startDate: new Date(2025, 6, 4).getTime(),
+    endDate: new Date(2025, 7, 21).getTime(),
   },
   {
-    startDate: new Date(2024, 6, 29).getTime(),
-    endDate: new Date(2024, 8, 13).getTime(),
+    startDate: new Date(2025, 6, 7).getTime(),
+    endDate: new Date(2025, 7, 22).getTime(),
   },
 ]
